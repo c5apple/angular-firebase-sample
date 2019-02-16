4a900237f2879bd60effe62d047622e7d173b703
c5apple <c5apple_party@yahoo.co.jp>
2019-02-16 15:20:45 +0900

    Firestore 取得

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-firebase-sample';
+export class AppComponent implements OnInit {
+
+  items: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) { }
+
+  ngOnInit() {
+    this.items = this.db.collection('items').valueChanges();
+  }
 }
